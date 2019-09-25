df8def7ca5629233f1d2f4776b873b505408c4a2
Alice Alsford <alice.alsford@gmail.com>
2019-09-25 19:55:06 +1200

    added attractions

--- main.js
+++ main.js
@@ -8,18 +8,23 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(mymap);
 
 
-function addMarker(long, lat, message) {
-    L.marker([long, lat]).addTo(mymap).bindPopup(message);
+var photo2 = "porirua.jpeg"
+
+function addMarker(long, lat, name, subtitle, photo, website) {
+    L.marker([long, lat]).addTo(mymap).bindPopup(`<b><h2>${name}</h2></b>${subtitle}<br><br> 
+    <img src="/images/${photo}" height="140px" width="180px"/> <br><br>For opening hours and ticket prices:<br> <a href=${website} target="_blank">Visit Website</a>`);
 }
 
-addMarker(-41.1351, 174.8391, "<b>Welcome to Porirua</b><br>The city of the future");
-addMarker(-41.1351, 174.88, "New");
-addMarker(-41.132984, 174.838207, "Te Rauparaha Arena");
-addMarker(-41.119978, 174.856833, "Aotea Lagoon");
+addMarker(-41.13158, 174.83352, "Awesome Bounce", "Childrens playland and cafe, bouncy castles, indoor trampolines and climbing wall", "bounce.jpg", "https://www.awesomebounce.co.nz/");
+addMarker(-41.1351, 174.88, "Test", "This is a test", "pools.jpeg", "www.google.com");
+addMarker(-41.1351, 174.88, "Pirate's Cove", "Putt through caves, past waterfalls, through tombstones, around shark infested waters, and on a nice day check out the beautiful sea views as you putt around the pirate mini golf course!", "pirate.jpg", "http://www.adventuregolf.co.nz/locations/pirates-cove-wellington/");
+addMarker(-41.122587, 174.853852, "Adrenalin Forest", "Adrenalin Forest is a series of rope bridges, Tarzan swings and flying foxes from platforms constructed in the tree canopy", "adrenalin forest.jpeg", "http://www.adrenalin-forest.co.nz/wellington");
+addMarker(-41.132984, 174.838207, "Te Rauparaha Arena", "Porirua's biggest aquatic centre", "pools.jpeg", "https://terauparaha-arena.co.nz/     aquatics/visit-arena-pool/");
+addMarker(-41.119978, 174.856833, "Aotea Lagoon", "Aotea Lagoon features a huge playground complete with a separate toddlers’ area, picnic      and BBQ areas, and an awesome splash pad. All free except for the train", "aotea.jpg", "https://poriruacity.govt.nz/discover-porirua/parks-and-reserves/aotea-lagoon/");
 
 var popup = L.popup()
     .setLatLng([-41.1351, 174.8391])
-    .setContent("<b>Welcome to Porirua</b><br>The city of the future<br>Explore this page to see all the fun activities for you and your family")
+    .setContent(`<b>Welcome to Porirua</b><br><br>The city of the future<br>Explore this page to see all the fun activities for you and your family<br><br><img src="/images/${photo2}" height="240px" width="280px"/>`)
     .openOn(mymap);
 
 function onMapClick(e) {
@@ -30,3 +35,4 @@ function onMapClick(e) {
 }
 
 mymap.on('click', onMapClick);
+
