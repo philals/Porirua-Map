3983ac548947a10c1b7ae3301497e8b3f36745b4
Phil Alsford <phil.alsford@gmail.com>
2019-10-02 21:16:32 +1300

    images should work in gh-pages

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 var mymap = L.map('mapid').setView([-41.1351, 174.8391], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiYWxpY2VhbHMiLCJhIjoiY2swdXE1YjFjMHFwMzNucHI5cGI5dmp0ayJ9.o3PFbQB_zSPIAqzelNUudw'
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoiYWxpY2VhbHMiLCJhIjoiY2swdXE1YjFjMHFwMzNucHI5cGI5dmp0ayJ9.o3PFbQB_zSPIAqzelNUudw'
 }).addTo(mymap);
 
 
 var photo2 = "porirua.jpeg"
 
 function addMarker(long, lat, name, subtitle, photo, website) {
-    L.marker([long, lat]).addTo(mymap).bindPopup(`<b><h2>${name}</h2></b>${subtitle}<br><br> 
-    <img src="images/${photo}" height="140px" width="180px"/> <br><br>For opening hours and ticket prices:<br> <a href=${website} target="_blank">Visit Website</a>`);
+  L.marker([long, lat]).addTo(mymap).bindPopup(`<b><h2>${name}</h2></b>${subtitle}<br><br> 
+    <img src="./images/${photo}" height="140px" width="180px"/> <br><br>For opening hours and ticket prices:<br> <a href=${website} target="_blank">Visit Website</a>`);
 }
 
 addMarker(-41.13158, 174.83352, "Awesome Bounce", "Childrens playland and cafe, bouncy castles, indoor trampolines and climbing wall", "bounce.jpg", "https://www.awesomebounce.co.nz/");
@@ -31,15 +31,15 @@ addMarker(-41.132984, 174.838207, "Te Rauparaha Arena", "Porirua's biggest aquat
 addMarker(-41.119978, 174.856833, "Aotea Lagoon", "Aotea Lagoon features a huge playground complete with a separate toddlers’ area, picnic      and BBQ areas, and an awesome splash pad. All free except for the train", "aotea.jpg", "https://poriruacity.govt.nz/discover-porirua/parks-and-reserves/aotea-lagoon/");
 
 var popup = L.popup()
-    .setLatLng([-41.1351, 174.8391])
-    .setContent(`<h1><b>Welcome to Porirua</h1></b>Explore this page to see all the fun activities for you and your family. Click on the pins to find out more information about many great activies for families in Porirua.<br><br><img src="images/${photo2}" height="240px" width="280px"/>`)
-    .openOn(mymap);
+  .setLatLng([-41.1351, 174.8391])
+  .setContent(`<h1><b>Welcome to Porirua</h1></b>Explore this page to see all the fun activities for you and your family. Click on the pins to find out more information about many great activies for families in Porirua.<br><br><img src="images/${photo2}" height="240px" width="280px"/>`)
+  .openOn(mymap);
 
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(mymap);
 }
 
 mymap.on('click', onMapClick);
